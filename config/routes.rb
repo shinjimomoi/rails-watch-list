1f4751da6ffffd862a184a8489ae4b7414c61b96
Ricardo Momoi <shinjimomoi@gmail.com>
2022-05-13 12:30:38 +0900

    Deploying on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
+  # trying to commit
   resources :bookmarks, only: :destroy
 end
